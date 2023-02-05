175edf181de30c8111ea2f310d8b4590aa882681
Leo Tremblay <leotrem2567@gmail.com>
2023-02-04 23:16:26 -0500

    modified sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,17 +2,30 @@ import React from 'react';
 import { Box, Slider, Stack, Typography } from '@mui/material'
 import { Thermostat, Air, LocalDrink, Add, Remove } from '@mui/icons-material';
 import './Sidebar.css'
+import TopSideBar from './TopSideBar';
 
 function Sidebar(props) {
+    const changeTemp = (event, temp) => {
+        props.setTemp(temp);
+    }
+    const changeWind = (event, wind) => {
+        props.setWind(wind);
+    }
+    const changeHumidity = (event, humidity) => {
+        props.setHumidity(humidity);
+    }
     return (
         <>
             <Stack className="container" direction="column" alignItems="center" spacing={5}>
+                <TopSideBar />
                 <Stack className="section" direction="column" alignItems="center" spacing={2}>
                     <Typography><Thermostat />  Temperature</Typography>
                     <Stack className="slider" direction="row" spacing={2}>
                         <Remove />
                         <Slider 
                         defaultValue={0}
+                        value={props.temp}
+                        onChange={changeTemp}
                         valueLabelDisplay="auto"
                         step={1}
                         marks
@@ -28,6 +41,8 @@ function Sidebar(props) {
                         <Remove />
                         <Slider 
                         defaultValue={0}
+                        value={props.wind}
+                        onChange={changeWind}
                         valueLabelDisplay="auto"
                         step={1}
                         marks
@@ -43,6 +58,8 @@ function Sidebar(props) {
                         <Remove />
                         <Slider 
                         defaultValue={0}
+                        value={props.humidity}
+                        onChange={changeHumidity}
                         valueLabelDisplay="auto"
                         step={1}
                         marks
