28bd22e6179acdf8214d800ab350ac97bf5276b8
Samuel Maltais <leumasmontreal@hotmail.com>
2023-02-04 19:42:23 -0500

    Arranged absolute values

--- src/App.js
+++ src/App.js
@@ -7,15 +7,13 @@ import Canada from './Map-Component/Province';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <svg height={1000} width={1000}> <circle cx="120" cy="85" r="400" fill="#0b2b57" className='Circle'/></svg>
-        <Logo className="Logo"/>
-      </header>
-      <body>
-        <Canada/>
-      </body>
+    <>
+    < svg height={1000} width={1000} className='Circle'> <circle cx="120" cy="85" r="400" fill="#0b2b57" className='Circle'/></svg>
+    <Logo className="Logo"/>
+    <div className="App"> 
+      <Canada className="map"/>
     </div>
+    </>
   );
 }
 
